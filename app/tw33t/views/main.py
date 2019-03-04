f4f4ef40b69f24fccaa2351e902a1340a6d73ef4
Tin <thanhtinpk092007@gmail.com>
2019-03-04 12:28:59 +0700

    Add url router get twitter handle

--- app/tw33t/views/main.py
+++ app/tw33t/views/main.py
@@ -5,6 +5,8 @@ from functools import wraps
 from flask import jsonify
 from twitter import *
 import json, requests, datetime, sys, os, uuid, re, time
+from flask.views import MethodView
+
 
 @app.route("/", methods=['GET'])
 def index():
@@ -12,8 +14,9 @@ def index():
     return render_template('index.html')
 
 
-'''
+class UserAPI(MethodView):
+    def get(self, twitter_handle=None):
+        return jsonify()
 
-Introduce a "Get tweets" route for the client and log relevant info from each search into a file.
 
-'''
+app.add_url_rule('/users/<string:twitter_handle>', view_func=UserAPI.as_view('users'))
