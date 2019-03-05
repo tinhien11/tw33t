d532aadd6f71cf5327ce3084770dab55068f2440
Tin <thanhtinpk092007@gmail.com>
2019-03-05 11:21:14 +0700

    Home page UI

--- app/frontend/src/components/Home.vue
+++ app/frontend/src/components/Home.vue
@@ -1,41 +1,46 @@
 <template>
-  <div>
-    <p>Home page</p>
-
-    <form id="app" method="post" @submit.prevent="checkForm">
-      <div v-if="errors.length">
-        <b>Please correct the following error(s):</b>
-        <ul>
-          <li v-for="error in errors">{{ error }}</li>
-        </ul>
+  <div class="pusher">
+    <div class="ui vertical masthead center aligned segment">
+      <div class="ui container">
+        <!--<div class="ui large secondary menu">-->
+          <!--<a class="active item">Home</a>-->
+        <!--</div>-->
       </div>
-
-      <p>
-        <label for="name">Name</label>
-        <input type="text" name="name" id="name" v-model="name">
-      </p>
-
-      <p>
-        <input type="submit" value="Submit">
-      </p>
-    </form>
-
-    <div>
-      {{tweets}}
+      <div class="ui text container">
+        <h1 class="ui header">
+          <img src="../assets/logo.png">
+        </h1>
+        <h2>Get tweets by twitter handle</h2>
+        <form method="get" @submit.prevent="checkForm">
+          <div v-if="errors.length">
+            <ul>
+              <div class="ui visible red message" v-for="error in errors">
+                <p>{{ error }}</p>
+              </div>
+            </ul>
+          </div>
+          <div class="ui big icon input" v-bind:class="{ loading: isLoading}">
+            <input type="text" placeholder="Enter a twitter handle" v-model="keyword">
+            <i class="search icon"></i>
+          </div>
+          <button class="ui huge primary button">Search <i class="right arrow icon"></i></button>
+        </form>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
   import axios from 'axios'
+  import './Home.css'
 
   export default {
     data() {
       return {
         errors: [],
-        name: null,
-        tweets: []
+        keyword: null,
+        tweets: [],
+        isLoading: false,
       }
     },
     methods: {
@@ -43,8 +48,9 @@
         this.tweets = this.getTweetsFromBackend()
       },
       getTweetsFromBackend() {
-        const username = this.name;
-        const path = `${process.env.API_URL}/tweets/?user=${username}`
+        const keyword = this.keyword;
+        const path = `${process.env.API_URL}/tweets/?user=${keyword}`;
+        let self = this;
         axios.get(path)
           .then(response => {
             this.tweets = response.data
@@ -52,18 +58,24 @@
           .catch(error => {
             this.errors.push('Error.');
           })
+          .then(function () {
+            self.isLoading = false;
+          });
       },
       checkForm() {
-        if (this.name) {
-          this.getTweets()
+        this.errors = [];
+        this.isLoading = true;
+        if (this.keyword) {
+          this.getTweets();
           return true;
         }
 
         this.errors = [];
 
-        if (!this.name) {
-          this.errors.push('Name required.');
+        if (!this.keyword) {
+          this.errors.push('Twitter handle is required.');
         }
+        this.isLoading = false;
       },
     },
     created() {
