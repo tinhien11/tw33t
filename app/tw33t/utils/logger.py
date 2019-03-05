f5db2831e9c00199299dcc24f13d80d8324cceb3
Tin <thanhtinpk092007@gmail.com>
2019-03-05 12:36:25 +0700

    Log file to folder logs

--- app/tw33t/utils/logger.py
+++ app/tw33t/utils/logger.py
@@ -1,8 +1,13 @@
 import logging
+import os
 from tw33t import app
 
 
 logger_twitter_handle = logging.getLogger(__name__)
 logger_twitter_handle.setLevel(logging.DEBUG)
-f_handler = logging.FileHandler(app.config['TWITTER_HANDLE_FILENAME_LOG'])
+
+FOLDER_LOGS_PATH = app.root_path + '/logs/'
+os.makedirs(FOLDER_LOGS_PATH, exist_ok=True)
+
+f_handler = logging.FileHandler('%s%s' % (FOLDER_LOGS_PATH + app.config['TWITTER_HANDLE_FILENAME_LOG']))
 logger_twitter_handle.addHandler(f_handler)
