e2fd2eaa8a615d919ffd6293a6ce265512ae7bb4
Tin <thanhtinpk092007@gmail.com>
2019-03-05 12:36:48 +0700

    Fix syntax

--- app/tw33t/utils/logger.py
+++ app/tw33t/utils/logger.py
@@ -9,5 +9,5 @@ logger_twitter_handle.setLevel(logging.DEBUG)
 FOLDER_LOGS_PATH = app.root_path + '/logs/'
 os.makedirs(FOLDER_LOGS_PATH, exist_ok=True)
 
-f_handler = logging.FileHandler('%s%s' % (FOLDER_LOGS_PATH + app.config['TWITTER_HANDLE_FILENAME_LOG']))
+f_handler = logging.FileHandler('%s%s' % (FOLDER_LOGS_PATH, app.config['TWITTER_HANDLE_FILENAME_LOG']))
 logger_twitter_handle.addHandler(f_handler)
